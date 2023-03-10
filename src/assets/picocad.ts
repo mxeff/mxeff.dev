654398526d552a027ab51dff7cd548dd21e8cea1
max.franszkewitz <max.franszkewitz@check24.de>
2023-03-10 15:34:04 +0100

    add image assets

---
+++ src/assets/picocad.ts
@@ -0,0 +1,277 @@
+export default `picocad;mxeff;12;12;11
+{
+{
+ name='cylinder', pos={0,0,0}, rot={0,0,0},
+ v={
+  {1,-0.25,-0.25},
+  {0.75,-0.25,-1},
+  {-0.75,-0.25,-1},
+  {-1,-0.25,-0.25},
+  {-1,-0.75,0.75},
+  {-0.5,-1,1.25},
+  {0.5,-1,1.25},
+  {1,-0.75,0.75},
+  {0.5,0.5,-0.25},
+  {0.5,0.5,-0.75},
+  {-0.5,0.5,-0.75},
+  {-0.5,0.5,-0.25},
+  {-0.5,0,0},
+  {-0.5,0,0.25},
+  {0.5,0,0.25},
+  {0.5,0,0},
+  {1,-1.5,0},
+  {0.75,-1.75,-1},
+  {-0.75,-1.75,-1},
+  {-1,-1.5,0},
+  {-1,-1.25,0.75},
+  {-0.5,-1,1.25},
+  {0.5,-1,1.25},
+  {1,-1.25,0.75}
+ },
+ f={
+  {16,15,14,13,12,11,10,9, c=2, notex=1, uv={4.5,0.75,5,0.25,6,0.25,6.5,0.75,6.5,1.75,6,2.25,5,2.25,4.5,1.75} },
+  {1,9,10,2, c=2, uv={5.5,2.25,5.5,3,4.75,3,4.75,2.25} },
+  {2,10,11,3, c=2, uv={4.25,2.25,3.75,3,3,3,2.5,2.25} },
+  {3,11,12,4, c=2, uv={2,2.25,1.75,3,1.25,3,1.25,2.25} },
+  {4,12,13,5, c=2, uv={6.75,0,6.75,1,5.75,1,5.75,0} },
+  {5,13,14,6, c=2, uv={6.75,0,6.75,1,5.75,1,5.75,0} },
+  {6,14,15,7, c=2, uv={6.75,0,6.75,1,5.75,1,5.75,0} },
+  {7,15,16,8, c=2, uv={6.25,0.25,6.25,1.25,5.25,1.25,5.25,0.25} },
+  {8,16,9,1, c=2, uv={6.5,0.75,6.5,1.75,5.5,1.75,5.5,0.75} },
+  {17,1,2,18, c=2, uv={5.75,0.5,5.5,2.25,4.75,2.25,4.75,0.25} },
+  {18,2,3,19, c=2, uv={4.25,0.5,4.25,2.25,2.5,2.25,2.5,0.5} },
+  {19,3,4,20, c=2, uv={2,0.25,2,2.25,1.25,2.25,1,0.5} },
+  {20,4,5,21, c=2, uv={5.5,1,6.5,1,6.5,2,5.5,2} },
+  {21,5,6,22, c=2, uv={5,0,6,0,6,1,5,1} },
+  {23,7,8,24, c=2, uv={5.75,0,6.75,0,6.75,1,5.75,1} },
+  {24,8,1,17, c=2, uv={5.5,0,6.5,0,6.5,1,5.5,1} }
+ } 
+},{
+ name='pyramid', pos={0,-0.75,-2.25}, rot={0,0,0},
+ v={
+  {0,0.25,1},
+  {0,0.25,1},
+  {0.25,0.25,1.25},
+  {-0.25,0.25,1.25},
+  {0,-0.5,1.25}
+ },
+ f={
+  {1,2,3,4, c=8, uv={1.5,0.5,2.5,0.5,2.5,1.5,1.5,1.5} },
+  {5,2,1, c=8, uv={2,0.5,2.5,1.5,1.5,1.5} },
+  {5,3,2, c=8, notex=1, uv={2,0.5,2.5,1.5,1.5,1.5} },
+  {5,4,3, c=8, uv={2,0.5,2.5,1.5,1.5,1.5} },
+  {5,1,4, c=8, notex=1, uv={2,0.5,2.5,1.5,1.5,1.5} }
+ } 
+},{
+ name='cylinder', pos={-2.5,-3.5,0}, rot={0,0,0},
+ v={
+  {3,1,-0.25},
+  {2.75,1,-0.5},
+  {2.25,1,-0.5},
+  {2,1,-0.25},
+  {2,1,0.25},
+  {2.25,1,0.75},
+  {2.75,1,0.75},
+  {3,1,0.25},
+  {3.5,1.5,-0.25},
+  {3.25,1.5,-1},
+  {1.75,1.5,-1},
+  {1.5,1.5,-0.25},
+  {1.5,1.5,0.75},
+  {2,1.5,1.25},
+  {3,1.5,1.25},
+  {3.5,1.5,0.75},
+  {3.5,2.25,0.75},
+  {3,2.5,1.25},
+  {2,2.5,1.25},
+  {1.5,2.25,0.75},
+  {1.5,2,-0.25},
+  {1.75,1.75,-1},
+  {3.25,1.75,-1},
+  {3.5,2,-0.25}
+ },
+ f={
+  {1,2,3,4,5,6,7,8, c=1, notex=1, uv={11.5,0,12.5,0,13,0.5,13,1.5,12.5,2,11.5,2,11,1.5,11,0.5} },
+  {1,9,10,2, c=1, dbl=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {2,10,11,3, c=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {3,11,12,4, c=1, dbl=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {4,12,13,5, c=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {5,13,14,6, c=1, dbl=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {6,14,15,7, c=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {7,15,16,8, c=1, dbl=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {8,16,9,1, c=1, notex=1, uv={12.5,0.5,12.5,1.5,11.5,1.5,11.5,0.5} },
+  {17,16,15,18, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {18,15,14,19, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {19,14,13,20, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {20,13,12,21, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {21,12,11,22, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {22,11,10,23, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {23,10,9,24, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {24,9,16,17, c=1, notex=1, uv={11,0.5,12,0.5,12,1.5,11,1.5} },
+  {17,18,19,20,21,22,23,24, c=1, uv={11,0.5,11.5,0,12.5,0,13,0.5,13,1.5,12.5,2,11.5,2,11,1.5} }
+ } 
+},{
+ name='cube', pos={-1.75,-0.75,0.25}, rot={0,0,0},
+ v={
+  {0.75,-0.5,-0.5},
+  {0.75,-0.5,-0.5},
+  {0.75,0.25,-0.5},
+  {0.75,0.25,-0.5},
+  {0.5,-0.25,0},
+  {0.75,-0.25,0},
+  {0.75,0.25,-0.25},
+  {0.75,0.25,-0.25}
+ },
+ f={
+  {1,2,3,4, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {6,5,8,7, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {5,6,2,1, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {5,1,4,8, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {2,6,7,3, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {4,3,7,8, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} }
+ } 
+},{
+ name='cube', pos={0.5,-0.75,0.25}, rot={0,0,0},
+ v={
+  {0.5,-0.5,-0.5},
+  {0.5,-0.5,-0.5},
+  {0.5,0.25,-0.5},
+  {0.5,0.25,-0.5},
+  {0.5,-0.25,0},
+  {0.75,-0.25,0},
+  {0.5,0.25,-0.25},
+  {0.5,0.25,-0.25}
+ },
+ f={
+  {1,2,3,4, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {6,5,8,7, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {5,6,2,1, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {5,1,4,8, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {2,6,7,3, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {4,3,7,8, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} }
+ } 
+},{
+ name='plane', pos={-2.5,0,-2.75}, rot={0,0,0},
+ v={
+  {1.5,-1.75,1.5},
+  {2,-2,0.75},
+  {1.5,-1.5,2.5},
+  {1.75,-1.75,1.75}
+ },
+ f={
+  {3,4,2,1, c=8, dbl=1, noshade=1, notex=1, uv={13,0,15,0,15,2,13,2} }
+ } 
+},{
+ name='plane', pos={-1.75,-1.5,-2.75}, rot={0,0,0},
+ v={
+  {1.75,-0.5,0.75},
+  {2.25,-0.5,0.75},
+  {1.75,-0.25,1.75},
+  {2.5,-0.25,1.75}
+ },
+ f={
+  {3,4,2,1, c=8, dbl=1, noshade=1, notex=1, uv={13,0,15,0,15,2,13,2} }
+ } 
+},{
+ name='plane', pos={3,-0.25,-2.75}, rot={0,0,0},
+ v={
+  {-4,-0.25,1.5},
+  {-2,-0.25,1.5},
+  {-4,-1,1.5},
+  {-2,-1,1.5}
+ },
+ f={
+  {3,4,2,1, c=6, dbl=1, uv={9,1.25,11.25,1.25,11.25,2,9,2} }
+ } 
+},{
+ name='plane', pos={2.5,-1,-1.25}, rot={0,0,0},
+ v={
+  {-1.5,0.5,1},
+  {-1.5,-0.25,1},
+  {-1.5,0.5,-0.0},
+  {-1.5,-0.25,0}
+ },
+ f={
+  {3,4,2,1, c=6, dbl=1, uv={11,2,11,1.25,11.75,1.25,11.75,2} }
+ } 
+},{
+ name='plane', pos={2.5,-2.5,-2.75}, rot={0,0,0},
+ v={
+  {-3.5,2,2.5},
+  {-3.5,1.25,2.5},
+  {-3.5,2,1.5},
+  {-3.5,1.25,1.5}
+ },
+ f={
+  {3,4,2,1, c=6, dbl=1, uv={11,2,11,1.25,11.75,1.25,11.75,2} }
+ } 
+},{
+ name='plane', pos={-2,-1.5,-3}, rot={0,0,0},
+ v={
+  {1.5,-0.5,1},
+  {2,-0.5,1},
+  {1.25,-0.25,2},
+  {2,-0.25,2}
+ },
+ f={
+  {3,4,2,1, c=8, dbl=1, noshade=1, notex=1, uv={13,0,15,0,15,2,13,2} }
+ } 
+},{
+ name='plane', pos={-1.25,-1.5,-2.75}, rot={0,0,0},
+ v={
+  {1.75,-0.5,0.75},
+  {2.25,-0.25,1.5},
+  {2,-0.25,1.75},
+  {2.25,0,2.5}
+ },
+ f={
+  {3,4,2,1, c=8, dbl=1, noshade=1, notex=1, uv={13,0,15,0,15,2,13,2} }
+ } 
+},{
+ name='cube', pos={0,0.75,0.25}, rot={0,0,0},
+ v={
+  {-0.5,-0.5,-0.5},
+  {0.5,-0.5,-0.5},
+  {0.5,0,-0.5},
+  {-0.5,0,-0.5},
+  {-0.5,-1.25,0.5},
+  {0.5,-1.25,0.5},
+  {0.5,-0.5,0.25},
+  {-0.5,-0.5,0.25}
+ },
+ f={
+  {1,2,3,4, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {6,5,8,7, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {5,6,2,1, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {5,1,4,8, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {2,6,7,3, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} },
+  {4,3,7,8, c=11, uv={5.5,0.5,6.5,0.5,6.5,1.5,5.5,1.5} }
+ } 
+}
+}%
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb66665555666600000000
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb66665555666600000000
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb66665555666600000000
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb66665555666600000000
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffffffffffffffffffff97ffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffffffffffffffffffff97ffffffffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb66665555666600000000
+fffffffffffffffffffff44499fff9444fffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb66665555666600000000
+fffffffffffffffffffff999999999999fffffffffffffffffffffbbbbbbbbbbbbbbbbbbbbb000000000000bbbbbbbbbbbbbbbbbbbbb66665555666600000000
+ffffffffff22fffffff9ff3399999933ff9fffffff22ffffffffffbbbbbbbbbbbbbbbbbbbb0bbbb0bb0bbbb0bbbbbbbbbbbbbbbbbbbb66665555666600000000
+ffffffffff5599999999ff3399999933ff9999999955ffffffffff000000000000000000000bbbb0000bbbb00000000000000000000055556666555500000000
+ffffffffff222ffffff4994499999944994ffffff222ffffffffffbbbbbbbbbbbbbbbbbbbb0bbbb0bb0bbbb0bbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffff2429fffff2999999999999992fffff9222ffffffffffbbbbbbbbbbbbbbbbbbbb0bbb0bbbb0bbb0bbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffff24229ffff299ff999999fff92ffff92222ffffffffffbbbbbbbbbbbbbbbbbbbbb000bbbbbb000bbbbbbbbbbbbbbbbbbbbb55556666555500000000
+ffffffffff222229fff29ffff999994ff92fff922442ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff2224224ff229ff94444224922ff4222422ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff2425222ff0224922222ef9420ff2242222ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff2522424fff2299feeeef4422fff2254242ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff2242452fff05222ffff94250fff2242522ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff2222222ffff525242244252ffff2222222ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff4525545fffff2422222225fffff2522542ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff5225422ffffff02224450ffffff2255222ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+ffffffffff2452252ffffffff5250ffffffff5242245ffffffffffbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb00000000000000000000
+`;
